1d83ac1adb8fc4620a1cb0476a4c4d68d81ffe0e
Nabeel Munawar <sibyltechnologiespk@gmail.com>
2026-02-24 22:55:38 +0500

    rebrand: app title and publisher to SIBYLERP

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -1,19 +1,19 @@
 app_name = "erpnext"
-app_title = "ERPNext"
-app_publisher = "Frappe Technologies Pvt. Ltd."
-app_description = """ERP made simple"""
+app_title = "SibylERP"
+app_publisher = "Sibyl Technologies"
+app_description = """Automation Made Easy"""
 app_icon = "fa fa-th"
 app_color = "#e74c3c"
-app_email = "hello@frappe.io"
+app_email = "support@sibyl.pk"
 app_license = "GNU General Public License (v3)"
-source_link = "https://github.com/frappe/erpnext"
-app_logo_url = "/assets/erpnext/images/erpnext-logo.svg"
+source_link = "https://github.com/sibyltechnologies/sibylerp"
+app_logo_url = "/assets/erpnext/images/sibyl-logo.svg"
 app_home = "/desk"
 
 add_to_apps_screen = [
 	{
 		"name": app_name,
-		"logo": "/assets/erpnext/images/erpnext-logo.svg",
+		"logo": "/assets/erpnext/images/sibyl-logo.svg",
 		"title": app_title,
 		"route": app_home,
 		"has_permission": "erpnext.check_app_permission",
@@ -115,8 +115,8 @@ calendars = ["Task", "Work Order", "Sales Order", "Holiday List", "ToDo"]
 website_generators = ["BOM", "Sales Partner"]
 
 website_context = {
-	"favicon": "/assets/erpnext/images/erpnext-favicon.svg",
-	"splash_image": "/assets/erpnext/images/erpnext-logo.svg",
+	"favicon": "/assets/erpnext/images/sibyl-favicon.svg",
+	"splash_image": "/assets/erpnext/images/sibyl-logo.svg",
 }
 
 # nosemgrep
